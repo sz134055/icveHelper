8f8677b4a5357d47e97288bd81bc48dd69275feb
0x000000F4 <1340554713@qq.com>
2022-05-15 16:27:56 +0800

    [feat](PAGE)INDEX，LOGIN页完善

--- WebClient/web/static/js/index.js
+++ WebClient/web/static/js/index.js
@@ -1,3 +1,30 @@
+var now_user;
+function divSwitch(status,info) {
+    var info_box = $('.info_box');
+    //重置
+    info_box.find('div').remove();
+    if (status==='user'){
+        if (now_user===undefined||now_user!==info.id) {
+            var user_info_box = $('.user_info').clone();
+            user_info_box.removeAttr('style')
+            user_info_box.find('img').attr('src', info.header);
+            user_info_box.find('span.user_num').text(info.num);
+            user_info_box.find('span.user_id').text(info.id);
+            info_box.append(user_info_box);
+        }
+    }else if (status==='free'){
+        var now_free_box = $('.now_free_box').clone();
+        now_free_box.removeAttr('style');
+        info_box.append(now_free_box);
+
+    }else{
+        var ajax_info_box = $('.ajax_info_box').clone();
+        ajax_info_box.removeAttr('style');
+        info_box.append(ajax_info_box);
+    }
+
+}
+
 function get_status(){
     var req = new XMLHttpRequest();
     req.onreadystatechange = function () {
@@ -21,14 +48,16 @@ function get_user() {
     req.onreadystatechange = function () {
         if (req.readyState === 4) {
             var getInfo = JSON.parse(req.responseText);
-            var nowUser = document.getElementById('nowUser');
 
             if (getInfo.code === '1') {
-                nowUser.innerText = "序号：" + getInfo.info.num + " ID：" + getInfo.info.id;
+                divSwitch('user',getInfo.info)
             }else if(getInfo.code === '10'){
-                nowUser.innerText = '队列当前无用户';
+                divSwitch('free',{});
             }else {
-                nowUser.innerText = "获取失败";
+                let ajax_info_box = $('.ajax_info_box');
+                ajax_info_box.find('p.ajax_info_msg').text('获取失败');
+                ajax_info_box.find('p.ajax_info_tips').text('稍等片刻或刷新重试');
+                divSwitch('',{});
             }
 
         }
@@ -37,6 +66,7 @@ function get_user() {
     req.send();
 }
 window.onload = function (){
-    get_status();
-    get_user();
+    //get_status();
+    divSwitch('',{});
+    setInterval(get_user,5000);
 }
